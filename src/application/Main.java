9631f8c6797d7ace9a8664c930e1169e66687b39
Kanika <Kanika@10.0.0.193>
2022-07-19 21:31:36 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,18 +5,18 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-
+ 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = new VBox(); 
+			VBox root  = new VBox(); 
 			root.getChildren().add(new Label("Kanika's label"));
 			root.getChildren().add(new Label("Kanika's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Zawad's Project");
+			primaryStage.setTitle("Zawad's Project"); 
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
